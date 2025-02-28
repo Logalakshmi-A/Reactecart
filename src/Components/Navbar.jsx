9c69939b9c895f154140b653966973c1283a5863
Logalakshmi-A <logalakshmi91@gmail.com>
2025-02-28 23:13:57 +0530

    Add files via upload

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,10 +9,10 @@ function Navbar() {
   return (
     <>
     <nav className='flex justify-between items-center p-4 shadow-md h-20 sticky top-0 bg-gradient-to-r from-blue-300 to-green-300'>
-      <Link className='flex flex-row' to='/'><img className='w-20 h-20' src='../Logo.png'/></Link>
+      <Link className='flex flex-row' to='/'><img className='w-20 h-20' src='/Logo.png'/></Link>
       <div className='text-2xl font-semibold'>E - Shopping Portal</div>
       <div className='flex justify-around gap-5 shadow-md'>
-      <Link className='flex flex-row' to='/'><img className='w-20 h-20 rounded-full' src='../Home Logo.png'/></Link>
+      <Link className='flex flex-row' to='/'><img className='w-20 h-20 rounded-full' src='/Home Logo.png'/></Link>
         <Link className='text-2xl font-semibold' to='/Shop'>Products</Link>
         <Link className='text-2xl font-semibold relative' to='/Cart' >
         <PiShoppingCartLight size={30} className='pt-2'/>
