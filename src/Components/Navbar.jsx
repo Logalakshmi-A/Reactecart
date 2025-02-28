3630a271c281915bebd0d4ce0f8d45e0bab3e116
Logalakshmi-A <logalakshmi91@gmail.com>
2025-02-28 20:06:54 +0530

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,18 +2,17 @@ import React, { useContext, useState } from 'react'
 import {Link} from 'react-router-dom'
 import { PiShoppingCartLight } from "react-icons/pi";
 import {CartContext} from '../Context/CartContext'
-import logo from '../assets/Logo.png';
-import homeLogo from '../assets/Home Logo.png';
+
 
 function Navbar() {
   const {cartItems} = useContext(CartContext);
   return (
     <>
     <nav className='flex justify-between items-center p-4 shadow-md h-20 sticky top-0 bg-green-200'>
-      <Link className='flex flex-row' to='/'><img className='w-20 h-20' src={logo}/></Link>
+      <Link className='flex flex-row' to='/'><img className='w-20 h-20' src='./Logo.png'/></Link>
       <div className='text-2xl font-semibold'>E - Shopping Portal</div>
       <div className='flex justify-around gap-5 shadow-md'>
-      <Link className='flex flex-row' to='/'><img className='w-10 h-10 rounded-full' src={homeLogo}/></Link>
+      <Link className='flex flex-row' to='/'><img className='w-20 h-20 rounded-full' src='./Home Logo.png'/></Link>
         <Link className='text-2xl font-semibold' to='/Shop'>Products</Link>
         <Link className='text-2xl font-semibold relative' to='/Cart' >
         <PiShoppingCartLight size={30} className='pt-2'/>
